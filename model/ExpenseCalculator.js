151ba373a9f68d3d1fba83bacf77fa985fea1750
SkelsT <emasuarez12@gmail.com>
2025-08-01 17:04:51 -0300

    model expenseCalculator

---
+++ model/ExpenseCalculator.js
@@ -0,0 +1,42 @@
+class ExpenseCalculator {
+    constructor() {
+        this.monthExpenses = {};
+    }
+
+    
+
+    addExpenseToMonth(monthId, expense) {
+        if (!this.monthExpenses[monthId]) {
+            this.monthExpenses[monthId] = new MonthExpense(monthId);
+        }
+        if (!(expense instanceof Expense)) {
+            throw new Error("No es una instancia de Expense");
+        }
+        this.monthExpenses[monthId].addExpense(expense);
+    }
+
+    calculateMonthlyExpense(monthId) {
+        this.ensureMonthId(monthId) 
+        return this.monthExpenses[monthId].calculateTotalExpenses();
+        
+    }
+
+    calculateMonthlyExpenseByCategory(monthId, category) {
+        this.ensureMonthId(monthId)
+        return this.monthExpenses[monthId].calculateExpensesByCategory(category);
+    }
+    
+
+    listExpensesByMonth(monthId) {
+        this.ensureMonthId(monthId) 
+        return this.monthExpenses[monthId].getAllExpenses();
+    }
+    
+    
+    ensureMonthId(monthId) {
+    if (!this.monthExpenses[monthId]) {
+            throw new Error("No hay gastos registrados para este mes");
+        }
+    }
+
+}
